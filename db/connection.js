bdb5d7b9c7313310e5a2ff644f71a890731a5a35
lancechincodes <lanceechin@gmail.com>
2022-08-25 23:49:20 -0500

    Set up connection.js (connect to db)

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+// Require dotenv to read env variables from a .env file 
+require('dotenv').config()
+// Import Mongoose to interface with MongoDB
+const mongoose = require('mongoose')
+
+const mongoURI = process.env.DATABASE_URL
+const db = mongoose.connection
+
+// Connect to mongoDB with mongoURI
+mongoose.connect(mongoURI)
+
+// Connection messages
+db.on('error', (err) => console.log(err.message))
+db.on('connected', () => console.log('mongo connected at: ', mongoURI))
+db.on('disconnected', () => console.log('mongo disconnected'))
+
+db.on('open', () => {
+    console.log('Mongo connection made ✅')
+})
+
+module.exports = mongoose
